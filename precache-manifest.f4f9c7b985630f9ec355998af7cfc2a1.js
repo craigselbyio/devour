76e7d11d86881aa9b4d6f2adc97badf9a5dcefda
Craig Selby <craigselby@att.net>
2018-12-21 00:08:20 -0500

    Updates

---
+++ precache-manifest.f4f9c7b985630f9ec355998af7cfc2a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0fddbc5967e4c7ebcd0958a96973449",
+    "url": "/devour/static/media/background_search_mobile.b0fddbc5.jpg"
+  },
+  {
+    "revision": "5890de17aa2d8f8bfb95bd659d3bdc69",
+    "url": "/devour/static/media/background_search_desktop.5890de17.jpg"
+  },
+  {
+    "revision": "656d6dfca0792c2e560b",
+    "url": "/devour/static/js/runtime~main.656d6dfc.js"
+  },
+  {
+    "revision": "827a394d04fea3c9b1d8",
+    "url": "/devour/static/js/main.827a394d.chunk.js"
+  },
+  {
+    "revision": "62d8ccf9ea270606c305",
+    "url": "/devour/static/js/1.62d8ccf9.chunk.js"
+  },
+  {
+    "revision": "827a394d04fea3c9b1d8",
+    "url": "/devour/static/css/main.e356d45f.chunk.css"
+  },
+  {
+    "revision": "9dc3362c87d4d257481156cb7451f9c2",
+    "url": "/devour/index.html"
+  }
+];
